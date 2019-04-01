a0ded7c6634898810ed54c5fbfb2e842988b063f
2MarShmello2 <44773337+2MarShmello2@users.noreply.github.com>
2019-04-01 18:56:22 +0300

    try print

--- Tree.cpp
+++ Tree.cpp
@@ -15,6 +15,7 @@ _root = nullptr;
 }
 void Tree::insert(Node *root,int data)
 { 
+	cout << "insert" << endl;
    //check if the number already exist
    if(contains(root,data)){throw std::invalid_argument( "number is already exist" );return;}
     
