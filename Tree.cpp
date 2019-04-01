e645170e9be04f382c0680b05c198a311440d7f1
2MarShmello2 <44773337+2MarShmello2@users.noreply.github.com>
2019-04-01 19:15:32 +0300

    Update Tree.cpp

--- Tree.cpp
+++ Tree.cpp
@@ -204,9 +204,9 @@ int Tree::parent(Node* root, int data)
 	cout << "parent" << endl;
 		cout << data << endl;
 
-    if(!contains(root,data)){throw std::invalid_argument( "number is not exist" );return -1;}
+    if(!contains(root,data)){throw std::invalid_argument( "number is not exist" );}
     if(root->left == nullptr && root->right == nullptr)
-       throw std::invalid_argument( "no parent,its the root" );return -1;
+       throw std::invalid_argument( "no parent,its the root" );
     int currentVal = root->data;	
     if( (root->left != nullptr && root->left->data == data)
         || (root->right != nullptr && root->right->data == data))
